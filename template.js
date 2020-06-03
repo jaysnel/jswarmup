3d5d90309f53b0d8bab9d5adcc4a45dc8e32f3d0
Jaylan Snelson <jaylan210@gmail.com>
2020-06-03 16:32:12 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,42 @@
+// Arrays
+
+// Write a function, called forLoop that takes an array as a parameter, runs the array through a for(...) loop and does a console.log() of each element.
+
+
+// Write a function, called whileLoop that takes an array as a parameter, runs the array through a while(...) loop and does a console.log() of each element.
+
+
+// Implement .map(), .filter(), .reduce() as regular javascript functions that take in an array as a parameter, but do the same things as the real functions.
+//Map
+
+//Filter
+
+//Reduce
+
+// Start Of Objects
+
+// Begin with the starter code below ...
+let people = ['Kookla','Fran','Ollie'];
+
+let stuff = {
+  tv: 'huge',
+  radio: 'old',
+  toothbrush: 'frayed',
+  cars: ['Toyota','Mazda']
+}
+
+// Using spread and destructuring assignment, create a new array called `newPeople', which is a copy of the `people` array, with a person named 'Odie' added to the beginning and one named 'Garfield' added to the end.
+
+
+// Using spread and destructuring assignment, create a new object called `newStuff', which is a copy of the `stuff` object, with a new car added to the end of the `cars` array within it
+
+
+// Create a `state` object with keys of people and stuff that contain the `people` and `stuff` data.
+// Do this using object destructuring assignment
+
+
+// Using spread and destructuring assignments, create a new object called `newSate`, repeating the `newPeople` and `newStuff` steps above but directly within the people and stuff nodes of the state object (don't just spread in `newPeople` and `newStuff`)
+
+// Prove that the original people, stuff, and state are unchanged.
+//console.log(people);
+//console.log(stuff);
